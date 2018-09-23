e112da869f43dd1dd456cb48dee67ce3182e69cb
Yiyang Zhu <ererandkeke@gmail.com>
2018-09-23 00:03:33 -0400

    s

--- ZigzagTraversal.java
+++ ZigzagTraversal.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class ZigzagTraversal {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+    }
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        List<Integer> list1 = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> currentLevel = new Stack<>();
+        Stack<TreeNode> nextLevel = new Stack<>();
+        currentLevel.push(root);
+        boolean leftToRight = true;
+
+        while (!currentLevel.isEmpty()) {
+            TreeNode node = currentLevel.pop();
+            list1.add(node.val);
+            result.add(list1);
+            if (leftToRight) {
+                if (node.left != null) {
+                    nextLevel.push(node.left);
+                }
+                if (node.right != null) {
+                    nextLevel.push(node.right);
+                }
+            } else {
+                if (node.right != null) {
+                    nextLevel.push(node.right);
+                }
+                if (node.left != null) {
+                    nextLevel.push(node.left);
+                }
+            }
+            if (currentLevel.isEmpty()) {
+                leftToRight = !leftToRight;
+                Stack<TreeNode> temp = currentLevel;
+                currentLevel = nextLevel;
+                nextLevel = temp;
+            }
+
+        }
+        return result;
+    }
 }
