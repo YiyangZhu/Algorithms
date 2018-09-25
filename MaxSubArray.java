4ad10c924f7125ef9f832407351b49476be58ef5
Yiyang Zhu <ererandkeke@gmail.com>
2018-09-25 13:18:25 -0400

    use dp

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(maxSubArray(array));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int max = nums[0];
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = Math.max(nums[i], nums[i] + sum[i - 1]);
+            max = Math.max(sum[i], max);
+        }
+        return max;
+    }
 }
---
+++ StringOddNumber.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class StringOddNumber {
+}
