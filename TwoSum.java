419603a96afe08da5d793c9b42dd9751c343c558
Yiyang Zhu <ererandkeke@gmail.com>
2018-10-25 14:56:28 -0400

    s

--- TwoSum.java
+++ TwoSum.java
@@ -1,29 +1,18 @@
 import java.util.HashMap;
-import java.util.HashSet;
 
-public class TwoSum {
-    public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        System.out.println(twoSum(nums,target));
-
-    }
-
-    public static int[] twoSum(int[] nums, int target) {
+public class TwoSum{
+    public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        int index1 = -1;
-        int index2 = -1;
+        int[] result = new int[2];
         for(int i = 0; i < nums.length;i++){
-            if(map.containsKey(target-nums[i])){
-                index1 = map.get(target-nums[i]);
-                index2 = i;
-                break;
+            if(map.containsKey(target - nums[i])){
+                result[0] = map.get(target - nums[i]);
+                result[1] = i;
+                return result;
+            } else {
+                map.put(nums[i],i );
             }
-            map.put(nums[i],i);
         }
-        int[] result = {index1,index2};
-        System.out.println(index1);
-        System.out.println(index2);
         return result;
     }
 }
