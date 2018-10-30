fb70dbdde50d8b2d5a0dbaf84982b0671ba5833c
Yiyang Zhu <ererandkeke@gmail.com>
2018-10-30 19:39:05 -0400

    s

--- QuickSort.java
+++ QuickSort.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class QuickSort {
+    public int[] quickSort(int[] nums){
+        quick(nums,0,nums.length-1);
+        return nums;
+    }
+
+    public void quick(int[] nums,int start, int end){
+        if(start >= end){
+            return;
+        }
+        if(end - start == 1){
+            if(nums[start]>nums[end]){
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+            }
+            return;
+        }
+        int pivotIndex = end;
+        int pivot = nums[pivotIndex];
+        int i = start;
+        int j = start;
+        for(j = start;j < end;j++){
+            if(nums[j] < pivot){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                i++;
+            }
+        }
+        int temp = nums[i];
+        nums[i] = nums[pivotIndex];
+        nums[pivotIndex] = temp;
+        quick(nums,start ,i-1 );
+        quick(nums,i+1 ,end );
+
+    }
 }
