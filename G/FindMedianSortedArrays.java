6cb044319fedd528097f6371e8f79fcfee70c9d7
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-04 12:12:56 -0500

    add binary search method

--- G/FindMedianSortedArrays.java
+++ G/FindMedianSortedArrays.java
@@ -35,12 +35,59 @@ public class FindMedianSortedArrays{
         }
         if(( m + n ) % 2 != 0 ){
             int index = ( m + n ) / 2;
-            return combinedArray[index];
+            return (double)combinedArray[index];
         } else {
             int index1 = ( m + n ) / 2;
             int index2 = ( m + n ) / 2 - 1;
             return ((double)combinedArray[index1] + (double)combinedArray[index2]) / 2.0;
         }
+    }
+
+    public static double findMedianSortedArrays(int[] A, int[] B){
+        int m = A.length;
+        int n = B.length;
+        if((m + n) % 2 != 0){
+            return (double)findKth(A,B ,(m + n) / 2 , 0,m - 1, 0,n - 1 );
+        } else {
+            int index1 = (m + n) / 2;
+            int index2 = (m + n) / 2 - 1;
+            return (findKth(A,B ,index1 ,0 ,m - 1 ,0 ,n-1 )
+                    +findKth(A, B, index2, 0,m-1 ,0 ,n-1 )) * 0.5;
+        }
+    }
+
+    public static int findKth(int[] A, int[] B, int k, int aStart, int aEnd, int bStart,int bEnd){
+        int aLen = aEnd - aStart + 1;
+        int bLen = bEnd - bStart + 1;
+
+        if(aLen == 0){
+            return B[bStart + k];
+        }
+        if(bLen == 0){
+            return A[aStart + k];
+        }
+        if(k == 0){
+            return A[aStart] < B[bStart] ? A[aStart] : B[bStart];
+        }
+
+        int aMid = aLen * k / (aLen + bLen);
+        int bMid = k - aMid - 1;
+
+        aMid = aMid + aStart;
+        bMid = bMid + bStart;
+
+        if(A[aMid] > B[bMid]){
+            k = k - (bMid - bStart + 1);
+            aEnd = aMid;
+            bStart = bMid + 1;
+        } else {
+            k = k - (aMid - aStart + 1);
+            aStart = aMid + 1;
+            bEnd = bMid;
+        }
+
+        return findKth(A,B , k, aStart, aEnd, bStart, bEnd);
+
     }
 
 }
