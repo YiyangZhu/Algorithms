146beedd18630e55abfbf97eddfc3825f6c19aad
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-12 11:07:23 -0500

    shortest palindrome using recursion

---
+++ G/ShortestPalindrome.java
@@ -0,0 +1,24 @@
+package G;
+
+public class ShortestPalindrome {
+    public static String shortestPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(j >= 0){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+            }
+            j--;
+        }
+
+        if(i == s.length()){
+            return s;
+        }
+
+        String suffix = s.substring(i );
+        String prefix = new StringBuilder(suffix).reverse().toString();
+        String mid = shortestPalindrome(s.substring(0,i ));
+        return prefix + mid + suffix;
+    }
+}
---
+++ G/Trie.java
@@ -0,0 +1,5 @@
+package G;
+
+public class ImplementTrie {
+    
+}
