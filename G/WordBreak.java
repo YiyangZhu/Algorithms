cfaacb45eb3046149fa743215fdd28486316a8c8
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-13 13:11:20 -0500

    add DP

--- G/WordBreak.java
+++ G/WordBreak.java
@@ -3,7 +3,34 @@ package G;
 import java.util.List;
 
 public class WordBreak {
+
     public static boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] t = new boolean[s.length() + 1];
+        t[0] = true;
+
+        for(int i = 0; i < s.length(); i++){
+            if(!t[i]){
+                continue;
+            }
+
+            for(String a: wordDict){
+                int len = a.length();
+                int end = len + i;
+
+                if(end > s.length() || t[end]){
+                    continue;
+                }
+                if(s.substring(i,end ).equals(a)){
+                    t[end] = true;
+                }
+            }
+        }
+
+        return t[s.length()];
+    }
+
+
+    public static boolean wordBreakRecursion(String s, List<String> wordDict) {
         return wordBreakHelper(s,wordDict,0);
     }
 
