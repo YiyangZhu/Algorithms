59c8dc3c0b2234f709efd8d4548174462f830b1c
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-13 12:55:30 -0500

    using recursion, O(n) = n^2

--- G/WordBreak.java
+++ G/WordBreak.java
@@ -1,44 +1,33 @@
 package G;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.PriorityQueue;
 
 public class WordBreak {
-    static Comparator<String> comp = Comparator.comparing(String->String.length());
-    static PriorityQueue<String> queue;
     public static boolean wordBreak(String s, List<String> wordDict) {
-        if(s == null || wordDict == null){
-            return false;
-        }
-
-        queue = new PriorityQueue<>(comp.reversed());
-        for(String s1: wordDict){
-            queue.offer(s1);
-        }
-
-
-        return dfs(s,queue);
-
+        return wordBreakHelper(s,wordDict,0);
     }
 
-    public static boolean dfs(String s, PriorityQueue<String> wordDict){
-        if(s.equals("")){
+    public static boolean wordBreakHelper(String s, List<String> wordDict, int i){
+        if(i == s.length()){
             return true;
-        } else {
-            for(String word: wordDict){
-                int n = word.length();
-                String currentS = s;
-                if(s.length()>= n && s.substring(0,n ).equals(word)){
-                     if(dfs(s.substring(n),wordDict)){
-                         return true;
-                     }
-                }
-                s = currentS;
-            }
-            return false;
         }
 
-    }
+        for(String a: wordDict){
+            int len = a.length();
+            int end = len + i;
 
+            if(end > s.length()){
+                continue;
+            }
+            if(s.substring(i,end ).equals(a)){
+                if(wordBreakHelper(s,wordDict , end)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
 }
+
