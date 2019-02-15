c5a864af71a8ea89d3debcdfda73b560ead3c7a1
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-14 23:55:07 -0500

    using HashMap<String, index>

---
+++ G/MedianFinder.java
@@ -0,0 +1,21 @@
+package G;
+
+import java.util.PriorityQueue;
+
+public class FindMedianFromDataStream {
+    PriorityQueue<Integer> minHeap = null;
+    PriorityQueue<Integer> maxHeap = null;
+
+    public MedianFinder() {
+        minHeap = new PriorityQueue<>()
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+
+    }
+}
