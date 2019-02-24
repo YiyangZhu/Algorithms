c930aee37b6a7826300817d24b94d5e086e805fc
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-23 20:46:12 -0500

    using trie

--- Trie/ConcatenatedWords.java
+++ Trie/ConcatenatedWords.java
@@ -5,16 +5,16 @@ import java.util.*;
 public class ConcatenatedWords {
 
 
-    public static List<String> findConcatenation(String[] words){
+    public static List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> result = new ArrayList<>();
 
         Arrays.sort(words, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if(o1.length() > o2.length()){
+                if (o1.length() > o2.length()) {
                     return 1;
                 }
-                if(o1.length() < o2.length()){
+                if (o1.length() < o2.length()) {
                     return -1;
                 }
 
@@ -25,8 +25,8 @@ public class ConcatenatedWords {
         HashSet<String> set = new HashSet<>();
 
 
-        for(String s: words){
-            if(form(s, set)){
+        for (String s : words) {
+            if (form(s, set)) {
                 result.add(s);
             }
             set.add(s);
@@ -35,19 +35,19 @@ public class ConcatenatedWords {
         return result;
     }
 
-    public static boolean form(String word, HashSet<String> set){
-        if(set.isEmpty()){
+    public static boolean form(String word, HashSet<String> set) {
+        if (set.isEmpty()) {
             return false;
         }
         boolean[] dp = new boolean[word.length() + 1];
         dp[0] = true;
 
-        for(int i = 0; i < dp.length; i++){
-            for(int j = 0; j < i; j++){
-                if(!dp[j]){
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (!dp[j]) {
                     continue;
                 }
-                if(set.contains(word.substring(j,i ))){
+                if (set.contains(word.substring(j, i))) {
                     dp[i] = true;
                     break;
                 }
@@ -58,38 +58,63 @@ public class ConcatenatedWords {
         return dp[word.length()];
     }
 
-    public static List<String> findConcatenationTrie(String[] words){
+    public List<String> findAllConcatenatedWordsInADictUsingTrie(String[] words) {
         List<String> result = new ArrayList<>();
 
-        Trie trie = new Trie();
-        for(String s: words){
-            trie.insert(s);
+        TrieNode root = new TrieNode();
+
+        for (String s : words) {
+            addWord(s, root);
         }
 
-        int[] count = new int[1];
-        count[0] = 0;
-
-        for(String s: words){
-            count[0] = 0;
-            findComponentTrie(s, count, trie);
-            if(count[0] > 1){
+        for (String s : words) {
+            if (s.equals("")) {
+                continue;
+            }
+            if (find(s, 0, root, 0)) {
                 result.add(s);
             }
         }
         return result;
     }
 
-    public static int[] findComponentTrie(String word, int[] count, Trie trie){
-        for(int i = 0; i < word.length(); i++){
-            String left = word.substring(0,i );
-            String right = word.substring(i );
-
-            if(trie.search(left)){
-                count[0]++;
-                findComponentTrie(right,count,trie);
+    public void addWord(String s, TrieNode root) {
+        TrieNode node = root;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+            if (i == s.length() - 1) {
+                node.isWord = true;
             }
         }
-        return count;
+    }
 
+    public boolean find(String s, int index, TrieNode root, int count) {
+        TrieNode node = root;
+        int n = s.length();
+        for (int i = index; i < n; i++) {
+            char c = s.charAt(i);
+            if (node.children[c - 'a'] == null) {
+                return false;
+            }
+            if (node.children[c - 'a'].isWord) {
+                if (i == n - 1) {
+                    return count >= 1;
+                }
+                if (find(s, i + 1, root, count+1 )){
+                    return true;
+                }
+            }
+            node = node.children[c-'a'];
+        }
+        return false;
+    }
+
+    public class TrieNode {
+        boolean isWord;
+        TrieNode[] children = new TrieNode[26];
     }
 }
