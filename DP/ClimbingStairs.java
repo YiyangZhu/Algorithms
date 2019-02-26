d9e46d5c8c96a874d8bc17528628dd1d0d3d950a
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-26 09:15:58 -0500

    easy dp problem

---
+++ DP/ClimbingStairs.java
@@ -0,0 +1,16 @@
+package DP;
+
+public class ClimbingStairs {
+    public static int climbStairs(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        if(n == 1){
+            return dp[0];
+        }
+        dp[1] = 2;
+        for(int i = 2; i < n; i++){
+            dp[i] = dp[i - 1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
+}
