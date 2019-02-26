5f352d840dc2e8e464b8cff004f30a73cbfed07c
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-26 17:12:55 -0500

    min. cost climbing stairs, using dp, O(n) = n, extra space: O(n)

---
+++ DP/MinCostClimbingStairs.java
@@ -0,0 +1,27 @@
+package DP;
+
+public class MinCostClimbingStairs {
+    public static int minCostClimbingStairs(int[] cost) {
+        if (cost == null || cost.length == 0) {
+            return 0;
+        }
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        if (n == 1) {
+            return dp[0];
+        }
+
+        dp[1] = 0;
+        if (n == 2) {
+            return Math.min(dp[0], dp[1]);
+        }
+
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[n];
+
+    }
+}
