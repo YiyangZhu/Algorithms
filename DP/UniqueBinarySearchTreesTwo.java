dd8894062d1e84e6ec7ebe85cd982dc611c33316
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-27 16:34:07 -0500

    refer to Catalan number

---
+++ DP/UniqueBinarySearchTreesTwo.java
@@ -0,0 +1,31 @@
+package DP;
+
+public class UniqueBinarySearchTreesTwo {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int v) {
+            this.val = v;
+        }
+    }
+
+    public int numTrees(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        return dp[n];
+
+    }
+}
