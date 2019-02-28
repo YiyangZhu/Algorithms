10b3b0af032b39a9404e5c3c8c8144df065eb850
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-27 20:37:57 -0500

    solution to max. product subarray

--- DP/MaximumProductSubarray.java
+++ DP/MaximumProductSubarray.java
@@ -14,15 +14,41 @@ public class MaximumProductSubarray {
 
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                if(i == j){
+                if (i == j) {
                     dp[i][j] = nums[i];
                 } else {
-                    dp[i][j] = dp[i][j-1] * nums[j];
+                    dp[i][j] = dp[i][j - 1] * nums[j];
                 }
                 max = Math.max(max, dp[i][j]);
             }
         }
-
         return max;
+    }
+
+    public static int maxProductLessSpaceComplexity(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int n = nums.length;
+
+        int[] max = new int[n];
+        int[] min = new int[n];
+
+        max[0] = min[0] = nums[0];
+
+        int result = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0) {
+                max[i] = Math.max(nums[i], nums[i] * max[i - 1]);
+                min[i] = Math.min(nums[i], nums[i] * min[i - 1]);
+            } else {
+                max[i] = Math.max(nums[i], nums[i] * min[i - 1]);
+                min[i] = Math.min(nums[i], nums[i] * max[i - 1]);
+            }
+            result = Math.max(max[i], result);
+        }
+        return result;
     }
 }
