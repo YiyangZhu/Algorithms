2ad92a8f03e9dd8ae2506b3b1e82e9fc477a6e43
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-28 16:21:35 -0500

    dp, time complexity = n

--- DP/CountingBits.java
+++ DP/CountingBits.java
@@ -3,22 +3,19 @@ package DP;
 public class CountingBits {
     public int[] countBits(int num) {
         int[] result = new int[num + 1];
+        int p = 1;
+        int pow = 1;
 
-        for(int i = 0; i <= num; i++){
-            result[i] = count(i);
-        }
-
-        return result;
-    }
-
-    public int count(int i){
-        int res = 0;
-        while (i != 0){
-            if(i % 2 == 1){
-                res++;
+        for (int i = 1; i <= num; i++) {
+            if(i == pow){
+                result[i] = 1;
+                pow <<= 1;
+                p = 1;
+            } else {
+                result[i] = result[p] + 1;
+                p++;
             }
-            i /= 2;
         }
-        return res;
+        return result;
     }
 }
