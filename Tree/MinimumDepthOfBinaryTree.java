96e975b17558fca6245d1fe3e4dff99c8da05c74
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-06 21:27:50 -0500

    using dfs

---
+++ Tree/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,27 @@
+package Tree;
+
+public class MinimumDepthOfBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (left == 0 || right == 0) {
+            return left + right + 1;
+        } else {
+            return Math.min(left, right) + 1;
+        }
+
+    }
+}
