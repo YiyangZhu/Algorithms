04a279cca4e676e6632a9080ef971ca9ab753ad6
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-06 23:10:11 -0500

    check each node as the starting node of adding up to the sum

---
+++ Tree/PathSumIII.java
@@ -0,0 +1,28 @@
+package Tree;
+
+public class PathSumIII {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+
+        return help(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+
+    private int help(TreeNode node, int sum) {
+        if (node == null) {
+            return 0;
+        }
+        return (node.val == sum ? 1 : 0) + help(node.left, sum - node.val) + help(node.right, sum - node.val);
+    }
+}
