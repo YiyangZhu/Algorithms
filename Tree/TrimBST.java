38657e8f3d7dc7fcbde5b73dc4e2d82428cdfcc2
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-10 23:43:20 -0400

    use recursion

---
+++ Tree/TrimBST.java
@@ -0,0 +1,31 @@
+package Tree;
+
+public class TrimBST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val > R) {
+            return trimBST(root.left, L, R);
+        }
+
+        if (root.val < L) {
+            return trimBST(root.right, L, R);
+        }
+
+        root.left = trimBST(root.left, L, R);
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+}
