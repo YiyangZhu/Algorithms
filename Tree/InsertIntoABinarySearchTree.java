e55e0b5e1d3906bd6bdeed10f614f11499851310
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-15 16:23:30 -0400

    insert into a BST

---
+++ Tree/InsertIntoABinarySearchTree.java
@@ -0,0 +1,26 @@
+package Tree;
+
+public class InsertIntoABinarySearchTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        } else {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+}
+        
