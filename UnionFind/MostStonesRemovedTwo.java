1b7af95950ba16f969a2b26ba57ecc89f988c7d0
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-20 21:00:39 -0400

    use rooted tree, beats 84.57% runtime

--- UnionFind/MostStonesRemovedTwo.java
+++ UnionFind/MostStonesRemovedTwo.java
@@ -1,96 +1,83 @@
 package UnionFind;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class MostStonesRemovedTwo {
     public class TreeNode {
         TreeNode p;
-        int[] a;
+        int index;
         int rank;
 
         public TreeNode() {
 
         }
 
-        public TreeNode(int[] a) {
-            this.a = a;
+        public TreeNode(int i) {
+            this.index = i;
         }
     }
 
-    HashSet<TreeNode> set;
+    int count;
 
     public int removeStones(int[][] stones) {
-        this.set = new HashSet<>();
-        HashMap<Integer, TreeNode> map = new HashMap<>();
+        count = 0;
+        HashMap<Integer, TreeNode> rowMap = new HashMap<>();
+        HashMap<Integer, TreeNode> colMap = new HashMap<>();
 
         for (int[] a : stones) {
             int i = a[0];
             int j = a[1];
             int[] a1 = {i, j};
             int index = a[0] * 531 + a[1];
-            TreeNode t1 = new TreeNode(a1);
-            map.put(index, t1);
+            TreeNode t1 = new TreeNode(index);
             MAKE_SET(t1);
-        }
-
-        for (int i = 0; i < stones.length - 1; i++) {
-            for (int j = i + 1; j < stones.length; j++) {
-                int[] a1 = stones[i];
-                int[] a2 = stones[j];
-                int i1 = a1[0];
-                int j1 = a1[1];
-                int i2 = a2[0];
-                int j2 = a2[1];
-                if (i1 == i2 || j1 == j2) {
-                    int index1 = i1 * 531 + j1;
-                    int index2 = i2 * 531 + j2;
-                    TreeNode t1 = FIND_SET(map.get(index1));
-                    TreeNode t2 = FIND_SET(map.get(index2));
-                    if (t1 != t2) {
-                        UNION(t1, t2);
-                    }
+            if (rowMap.containsKey(i)) {
+                TreeNode t2 = rowMap.get(i);
+                TreeNode p1 = FIND_SET(t1);
+                TreeNode p2 = FIND_SET(t2);
+                if (p1 != p2) {
+                    UNION(p1, p2);
                 }
+            } else {
+                rowMap.put(i, t1);
+            }
+
+            if (colMap.containsKey(j)) {
+                TreeNode t2 = colMap.get(j);
+                TreeNode p1 = FIND_SET(t1);
+                TreeNode p2 = FIND_SET(t2);
+                if (p1 != p2) {
+                    UNION(p1, p2);
+                }
+            } else {
+                colMap.put(j, t1);
             }
         }
-        return stones.length - set.size();
+        return stones.length - count;
     }
 
     private void MAKE_SET(TreeNode t1) {
         t1.p = t1;
         t1.rank = 0;
-        set.add(t1);
+        count++;
     }
 
     private TreeNode FIND_SET(TreeNode t1) {
-        if(t1 != t1.p){
+        if (t1 != t1.p) {
             t1.p = FIND_SET(t1.p);
         }
         return t1.p;
     }
 
     private void UNION(TreeNode t1, TreeNode t2) {
-        if(t1.rank > t2.rank){
+        if (t1.rank > t2.rank) {
             t2.p = t1;
-            HashSet<TreeNode> newSet = new HashSet<>();
-            for(TreeNode t: set){
-                if(t != t2){
-                    newSet.add(t);
-                }
-            }
-            set = newSet;
         } else {
             t1.p = t2;
-            if(t1.rank == t2.rank){
+            if (t1.rank == t2.rank) {
                 t2.rank++;
             }
-            HashSet<TreeNode> newSet = new HashSet<>();
-            for(TreeNode t: set){
-                if(t != t1){
-                    newSet.add(t);
-                }
-            }
-            set = newSet;
         }
+        count--;
     }
 }
