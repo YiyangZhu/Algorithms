40da7e52301ecf46bba7c7f7535bed9429950cf4
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-22 10:40:29 -0400

    use union find

--- UnionFind/AccountMerge.java
+++ UnionFind/AccountMerge.java
@@ -11,7 +11,7 @@ public class AccountMerge {
     private void demo() {
 
         List<List<String>> accounts = new ArrayList<>();
-        List<String> account1 = new ArrayList<>(Arrays.asList("David", "David0@m.co", "David1@m.co"));
+        List<String> account1 = new ArrayList<>(Arrays.asList("David", "David0@m.co", "David0@m.co", "David1@m.co"));
         List<String> account2 = new ArrayList<>(Arrays.asList("David", "David3@m.co", "David4@m.co"));
         List<String> account3 = new ArrayList<>(Arrays.asList("David", "David4@m.co", "David5@m.co"));
         List<String> account4 = new ArrayList<>(Arrays.asList("David", "David2@m.co", "David3@m.co"));
@@ -25,97 +25,80 @@ public class AccountMerge {
     }
 
     public class Node {
-        Node head;
-        Node next;
-        String s;
+        Node p;
+        int rank;
+        String name;
+        List<String> accounts;
 
-        Node(String s) {
-            this.s = s;
+        public Node(String s) {
+            this.name = s;
+        }
+    }
+
+    private void makeSet(Node n) {
+        n.p = n;
+        n.rank = 0;
+        n.accounts = new ArrayList<>();
+    }
+
+    private Node findSet(Node n) {
+        if (n != n.p) {
+            n.p = findSet(n.p);
+        }
+        return n.p;
+    }
+
+    private void union(Node n1, Node n2) {
+        if (n1.rank > n2.rank) {
+            n2.p = n1;
+        } else {
+            n1.p = n2;
+            if (n1.rank == n2.rank) {
+                n2.rank++;
+            }
         }
     }
 
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        Set<Node> names = new HashSet<>();
-        Set<Node> emailSet = new HashSet<>();
-        for (List<String> l : accounts) {
-            String name = l.get(0);
-            Node nameNode = new Node(name);
-            makeSet(nameNode);
-            names.add(nameNode);
-            for (int i = 1; i < l.size(); i++) {
-                Node eNode = new Node(l.get(i));
-                makeSet(eNode);
-                emailSet.add(eNode);
-                Node p1 = findSet(nameNode);
-                Node p2 = findSet(eNode);
-                if (p1 != p2) {
-                    union(p1, p2);
-
-                }
-            }
-        }
-
-        for (Node n1 : emailSet) {
-            for (Node n2 : emailSet) {
-                if (n1.s.equals(n2.s)) {
-                    Node p1 = findSet(n1);
-                    Node p2 = findSet(n2);
+        Map<String, Node> map = new HashMap<>();
+        List<Node> allNodes = new ArrayList<>();
+        for (List<String> current : accounts) {
+            String name = current.get(0);
+            Node n = new Node(name);
+            makeSet(n);
+            for (int i = 1; i < current.size(); i++) {
+                String email = current.get(i);
+                if (!map.containsKey(email)) {
+                    n.accounts.add(email);
+                    map.put(email, n);
+                } else {
+                    Node p1 = findSet(n);
+                    Node p2 = findSet(map.get(email));
                     if (p1 != p2) {
-                        unionNames(p1, p2);
-                        names.remove(p2);
+                        union(p1, p2);
                     }
                 }
+            }
+            allNodes.add(n);
+        }
+
+        for (Node n : allNodes) {
+            if (n.p != n) {
+                Node p1 = findSet(n.p);
+                p1.accounts.addAll(n.accounts);
             }
         }
 
         List<List<String>> result = new ArrayList<>();
-        for (Node n : names) {
-
-            Node head = n;
-            n = n.next;
-            Set<String> set1 = new HashSet<>();
-            while (n != null) {
-                set1.add(n.s);
-                n = n.next;
+        for (Node n : allNodes) {
+            if (n.p == n) {
+                List<String> l = new ArrayList<>();
+                l.add(n.name);
+                Collections.sort(n.accounts);
+                l.addAll(n.accounts);
+                result.add(l);
             }
-            LinkedList<String> list1 = new LinkedList<>(set1);
-            Collections.sort(list1);
-            list1.addFirst(head.s);
-            result.add(list1);
         }
         return result;
-    }
-
-    private void makeSet(Node n) {
-        n.head = n;
-    }
-
-    private Node findSet(Node n) {
-        return n.head;
-    }
-
-    private void union(Node n1, Node n2) {
-        Node tail = n1;
-        while (tail.next != null) {
-            tail = tail.next;
-        }
-        tail.next = n2;
-        while (n2 != null) {
-            n2.head = n1;
-            n2 = n2.next;
-        }
-    }
-
-    private void unionNames(Node n1, Node n2) {
-        n2 = n2.next;
-        Node tail = n1;
-        while (tail.next != null) {
-            tail = tail.next;
-        }
-        tail.next = n2;
-        while (n2 != null) {
-            n2.head = n1;
-            n2 = n2.next;
-        }
     }
 }
