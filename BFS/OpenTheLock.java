9771cd6e5db715e8ab8b42af3aef49578c7fc6bc
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-24 14:47:16 -0400

    refine codes, use bfs, beats 56.66% runtime

--- BFS/OpenTheLock.java
+++ BFS/OpenTheLock.java
@@ -28,120 +28,33 @@ public class OpenTheLock {
             int num = Integer.valueOf(numS);
             int count = Integer.valueOf(current[1]);
             count++;
-            if (numS.charAt(0) == '9') {
-                int result7 = enqueue(num - 9000, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num - 1000, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            } else if (numS.charAt(0) == '0') {
-                int result7 = enqueue(num + 1000, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num + 9000, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            } else {
-                int result7 = enqueue(num + 1000, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num - 1000, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            }
 
-            if (numS.charAt(1) == '9') {
-                int result7 = enqueue(num - 900, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
+            for (int i = 0; i < 4; i++) {
+                int tmp = 1;
+                if (i == 0) {
+                    tmp = 1000;
+                } else if (i == 1) {
+                    tmp = 100;
+                } else if (i == 2) {
+                    tmp = 10;
                 }
-                int result8 = enqueue(num - 100, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            } else if (numS.charAt(1) == '0') {
-                int result7 = enqueue(num + 100, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num + 900, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            } else {
-                int result7 = enqueue(num + 100, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num - 100, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            }
 
-            if (numS.charAt(2) == '9') {
-                int result7 = enqueue(num - 90, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num - 10, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            } else if (numS.charAt(2) == '0') {
-                int result7 = enqueue(num + 10, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num + 90, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            } else {
-                int result7 = enqueue(num + 10, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num - 10, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            }
+                int result1;
+                if (numS.charAt(i) == '9') {
+                    result1 = enqueue(num - 9 * tmp, t, count, min, checked, q, visited, set);
 
-            if (numS.charAt(3) == '9') {
-                int result7 = enqueue(num - 9, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
+                } else {
+                    result1 = enqueue(num + tmp, t, count, min, checked, q, visited, set);
+
                 }
-                int result8 = enqueue(num - 1, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
+                min = result1 >= 0 ? result1 : min;
+
+                if (numS.charAt(i) == '0') {
+                    result1 = enqueue(num + 9 * tmp, t, count, min, checked, q, visited, set);
+                } else {
+                    result1 = enqueue(num - tmp, t, count, min, checked, q, visited, set);
                 }
-            } else if (numS.charAt(3) == '0') {
-                int result7 = enqueue(num + 1, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num + 9, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
-            } else {
-                int result7 = enqueue(num + 1, t, count, min, checked, q, visited, set);
-                if (result7 >= 0) {
-                    min = result7;
-                }
-                int result8 = enqueue(num - 1, t, count, min, checked, q, visited, set);
-                if (result8 >= 0) {
-                    min = result8;
-                }
+                min = result1 >= 0 ? result1 : min;
             }
             visited[num] = true;
         }
@@ -149,7 +62,7 @@ public class OpenTheLock {
     }
 
     private int enqueue(int num2, int t, int count, int min, boolean[] checked, LinkedList<String[]> q, boolean[] visited, HashSet<Integer> set) {
-        if (num2 > 9999 || num2 < 0 || visited[num2] || set.contains(num2)) {
+        if (visited[num2] || set.contains(num2)) {
             return -1;
         }
         if (num2 == t) {
